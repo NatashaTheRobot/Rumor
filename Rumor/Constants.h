748aafc0c37badcba00e537993d1beefbb536cfc
Natasha Murashev <nmurashev@gmail.com>
2013-11-14 17:20:47 -0800

    added nearby friends and chat

---
+++ Rumor/Constants.h
@@ -0,0 +1,15 @@
+//
+//  Constants.h
+//  Rumor
+//
+//  Created by Natasha Murashev on 11/14/13.
+//  Copyright (c) 2013 NatashaTheRobot. All rights reserved.
+//
+
+
+#pragma mark - Parse Classes
+
+// User
+
+static NSString * const sParseClassUserKeyFacebookId        = @"facebookId";
+static NSString * const sParseClassUserKeyDisplayName       = @"displayName";
