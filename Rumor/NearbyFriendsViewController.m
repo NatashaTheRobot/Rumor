8e3cafff223a75dfe3a8c6a472a4d5fd620ff084
Natasha Murashev <nmurashev@gmail.com>
2013-11-14 16:43:33 -0800

    configured basic views in storyboards

---
+++ Rumor/NearbyFriendsViewController.m
@@ -0,0 +1,59 @@
+//
+//  NearbyFriendsViewController.m
+//  Rumor
+//
+//  Created by Natasha Murashev on 11/14/13.
+//  Copyright (c) 2013 NatashaTheRobot. All rights reserved.
+//
+
+#import "NearbyFriendsViewController.h"
+
+@interface NearbyFriendsViewController ()
+
+@end
+
+@implementation NearbyFriendsViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+
+@end
